fc7d847cfe827461aa153c486ea15eb9593d20c3
Alonso Manuel <galileomanuel@gmail.com>
2021-06-24 12:51:32 -0300

    Pruebas con Archivos

--- temasSegundoParcialProg3/Program.cs
+++ temasSegundoParcialProg3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace temasSegundoParcialProg3
 {
@@ -39,20 +40,67 @@ namespace temasSegundoParcialProg3
 
             int elem = 1;
 
-            while (elem != 0) 
+            while (elem != 0)
             {
                 elem = int.Parse(Console.ReadLine());
 
                 numeros.Add(elem);
             }
 
+            numeros.RemoveAt(numeros.Count - 1); //Elimina el ulimo elemento que es 0
+
             Console.WriteLine("Numeros ingresados: ");
 
-            foreach (int elemento in numeros) 
+            foreach (int elemento in numeros)
             {
                 Console.WriteLine(elemento);
+
+
             }
 
+            Console.WriteLine("====================================================");
+
+            //ARCHIVOS
+
+            Console.WriteLine("ARCHIVOS\n");
+
+            
+            //ESCRIBIR
+            try
+            {
+                string path = @"C:\Users\Manu\Documents\arch.txt";
+                Console.WriteLine("Escribi lo que quieras cabeza de pingo: ");
+                string texto = Console.ReadLine() + " -> Fecha y Hora: " + DateTime.Now;
+
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine(texto);
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //LEER 
+            Console.WriteLine("\n**********************************");
+            Console.WriteLine("LEYENDO ARCHIVO: \n");
+            
+            try
+            {
+                int counter = 0;
+                string line;
+                System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Manu\Documents\arch.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(line);
+                    counter++;
+                }
+
+                file.Close();
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+           
         }
     }
 }
